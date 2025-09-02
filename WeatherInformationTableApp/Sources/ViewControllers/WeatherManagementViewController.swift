59b337c762afa6adc920290a7321bf5b6a2e4a11
Takumi Hasegawa <t.hasegawa@renk.co.jp>
2025-09-02 10:37:29 +0900

    フォルダ整理

---
+++ WeatherInformationTableApp/Sources/ViewControllers/WeatherManagementViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WeatherManagementViewController.swift
+//  WeatherInformationTableApp
+//
+//  Created by TakumiHasegawa on 2025/09/02.
+//
+
+import UIKit
+
+class WeatherManagementViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
